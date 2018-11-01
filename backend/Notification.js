095b8209c52a6f3532b6b009ecd3e8e09ebd3a97
Tobias Bräutigam <tbraeutigam@gmail.com>
2018-11-01 14:41:39 +0100

    - use tags to describe database model in protos (index, edge names, etc.) - add channel clear method

--- backend/Notification.js
+++ backend/Notification.js
@@ -4,8 +4,13 @@
  * @author Tobias Bräutigam <tbraeutigam@gmail.com>
  * @since 2018
  */
-const createNotification = (message) => {
-  console.log(message)
+let logger
+
+const createNotification = (message, registry) => {
+  if (!logger) {
+    logger = registry.resolve('logger')(__filename)
+  }
+  logger.debug('create event notification from: ', JSON.stringify(message))
   let res = {
     phrase: 'New event in %s',
     content: message.start + ' ' + message.name,
